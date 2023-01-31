204ff6d86ade92e73a2d293bca8f29ae10dd53e4
Pin Chen <pamyc1876@gmail.com>
2023-01-31 01:59:54 -0500

    Display a list of all projects.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,14 @@
 const projectsData = [
-  {
+  { 
+    id: 23,
     title: "Fish-Pocket",
     description: "Fish Pocket is a friendly angler's app used to track fishing statistics! As an angler, you can log in with your Google account to access all of this app's features safely and conveniently.",
     image: "/images/Fish_Pocket.png",
     repositoryLink: "https://github.com/WarmSkin/Fish-Pocket.git",
     deploymentLInk: " https://fish-pocket.fly.dev",
   },
-  {
+  { 
+    id: 24,
     title: "Jellyfish-Hunting",
     description: "Jellyfish Hunting is a Snake-Game re-creation with a Spongebob theme! Wild jellyfish spawn randomly one at a time on the map. Spongebob follows the mouse icon when infront of him to catch each jellyfish. Spongebob can move in eight directions with animations and sound effects.",
     image: "/images/Jellyfish_Hunting.png",
@@ -14,6 +16,7 @@ const projectsData = [
     deploymentLInk: "https://spongebob-snake-game.netlify.app",
   },
   {
+    id: 25,
     title: "Tic-Tac-Toe",
     description: "A Tic-Tac-Toe game with a Minions theme. Users can play against each other or with a built-in AI.",
     image: "/images/Tic_Tac_Toe.png",
