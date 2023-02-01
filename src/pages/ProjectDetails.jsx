cfd0ea4095c246c461068ab6b275334068c6d815
Pin Chen <pamyc1876@gmail.com>
2023-02-01 01:11:51 -0500

    Fix a bug that the picture is not showing in the details page. Same as the two buttons.

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -4,10 +4,10 @@ function ProjectDetails () {
   return (
     <>
       <h1>{projectsData[0].title}</h1>
+      <img src={projectsData[0].image} alt="{projectsData[0].title}"/>
       <p>{projectsData[0].description}</p>
-      <img src="{projectsData[0].image}" alt="{projectsData[0].title}"/>
-      <a href="{projectsData[0].repositoryLink}"><button>GitHub</button></a>
-      <a href="{projectsData[0].deploymentLink}"><button>Open the App</button></a>
+      <a href={projectsData[0].repositoryLink}><button>GitHub</button></a>
+      <a href={projectsData[0].deploymentLink}><button>Open the App</button></a>
     </>
   )
 }
