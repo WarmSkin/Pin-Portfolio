9275918f56380afbfe2676d5fe4694b01dcb4026
Pin Chen <pamyc1876@gmail.com>
2023-02-01 21:03:39 -0500

    Add 'mailto:' to the mail link

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ function Contact() {
     <>
       <h1>Contact</h1>
       <p>
-        If you'd like to built an app together or just want to know me more, feel free to hit me up via <a href="pamyc1876@gmail.com">email</a>.
+        If you'd like to built an app together or just want to know me more, feel free to hit me up via <a href="mailto:pamyc1876@gmail.com">email</a>.
       </p>
       <p>You could also reach out to me through:</p>
       <ul>
